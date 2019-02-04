812a14f510d9b6f1e47dd86619e8dc0022a2d91a
hogehoge789 <hogehoge789@gmail.com>
2019-02-04 11:11:40 +0900

    first commit

--- create_vpc.go
+++ create_vpc.go
@@ -10,8 +10,10 @@ import (
 )
 
 var (
-	region           = "ap-northeast-1"
-	vpcCidr, vpcName = "172.18.152.0/24", "testaws01-vpnb1"
+	region                                     = "ap-northeast-1"
+	vpcCidr, vpcName                           = "172.1.152.0/24", "testaws01"
+	dhcpsetname, dhcpdomain, dhcpdns           = "testaws01-001", "ap-northeast-1.compute.internal", "AmazonProvidedDNS"
+	dhcpntp01, dhcpntp02, dhcpntp03, dhcpntp04 = "172.1.1.100", "172.1.1.101", "172.1.1.116", "172.1.1.117"
 )
 
 func main() {
@@ -39,4 +41,48 @@ func main() {
 		},
 		Resources: []*string{vpc.VpcId},
 	})
+
+	if err != nil {
+		log.Fatalln("03", err)
+	}
+
+	/*
+		// create dhcpset
+		createdhcp, err := client.CreateDhcpOptions(&ec2.CreateDhcpOptionsInput{
+			DhcpConfigurations: []*ec2.DhcpConfiguration{
+				&ec2.DhcpConfiguration{
+					Key: aws.String("domain-name"),
+					Values: []*string{
+						aws.String(dhcpdomain),
+					},
+				},
+				&ec2.DhcpConfiguration{
+					Key: aws.String("domain-name-servers"),
+					Values: []*string{
+						aws.String(dhcpdns),
+					},
+				},
+				&ec2.DhcpConfiguration{
+					Key: aws.String("ntp-servers"),
+					Values: []*string{
+						aws.String(dhcpntp01),
+						aws.String(dhcpntp02),
+						aws.String(dhcpntp03),
+						aws.String(dhcpntp04),
+					},
+				},
+			},
+		})
+
+		_, err = client.CreateTags(&ec2.CreateTagsInput{
+			Tags: []*ec2.Tag{
+				{Key: aws.String("Name"), Value: aws.String(vpcName)},
+			},
+			Resources: []*string{vpc.VpcId},
+		})
+
+	*/
+
+	// create subnet
+
 }
